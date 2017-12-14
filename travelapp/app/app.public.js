0b0fe569ac011a4b8481b1dc00fb204e3c5976db
Kabindra <Kabindra@KABINDRA>
2017-12-13 17:23:58 -0800

    index page work

---
+++ travelapp/app/app.public.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    'use strict';
+    window.APP = window.APP || {};
+    APP.NAME = "Myapp";
+    angular
+        .module(APP.NAME, ['ui.router', APP.NAME + '.routes']);
+
+})();
---
+++ travelapp/app/public/route.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+    var app = angular.module("Myapp" + '.routes', []);
+
+    app.config(_configureStates);
+
+    _configureStates.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+
+    function _configureStates($stateProvider, $locationProvider, $urlRouterProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false,
+        });
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state({
+                name: 'next',
+                url: '/next',
+                templateUrl: '/app/public/home.html',
+                title: 'next',
+                //controller: 'testController as testCtrl'
+            });
+    }
+    })();
